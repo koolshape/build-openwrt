d093ed4fc570d238633d3b5f8205d34634f9fe64
koolshape <142799393+koolshape@users.noreply.github.com>
2023-08-21 19:57:43 +0800

    Update diy-part2.sh

--- config/lede-master/diy-part2.sh
+++ config/lede-master/diy-part2.sh
@@ -19,7 +19,7 @@ sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package
 echo "DISTRIB_SOURCECODE='lede'" >>package/base-files/files/etc/openwrt_release
 
 # Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
-# sed -i 's/192.168.1.1/192.168.31.4/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
 # Replace the default software source
 # sed -i 's#openwrt.proxy.ustclug.org#mirrors.bfsu.edu.cn\\/openwrt#' package/lean/default-settings/files/zzz-default-settings
@@ -29,7 +29,11 @@ echo "DISTRIB_SOURCECODE='lede'" >>package/base-files/files/etc/openwrt_release
 # ------------------------------- Other started -------------------------------
 #
 # Add luci-app-amlogic
-svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
+# svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
+svn co https://github.com/kenzok8/small-package/tree/main/luci-app-passwall package/luci-app-passwall
+svn co https://github.com/kenzok8/small-package/tree/main/luci-app-quickstart package/luci-app-quickstart
+svn co https://github.com/kenzok8/small-package/tree/main/luci-app-istorex package/luci-app-istorex
+svn co https://github.com/kenzok8/small-package/tree/main/luci-app-dockerman package/luci-app-dockerman
 
 # Fix runc version error
 # rm -rf ./feeds/packages/utils/runc/Makefile
